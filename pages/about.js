a7beae5426b110a55eec2f2a135d1e1c01755740
irakevine <ikevine2016@gmail.com>
2023-07-19 16:42:39 +0200

    removing styles

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel tellus vitae ex tempor dapibus quis feugiat dolor. Proin at pulvinar lorem. Nunc at sapien iaculis, hendrerit quam eu, luctus mauris. Aenean faucibus justo nisi. Quisque eu lobortis ex. Donec pulvinar, est ac facilisis malesuada, velit diam lobortis dui,</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel tellus vitae ex tempor dapibus quis feugiat dolor. Proin at pulvinar lorem. Nunc at sapien iaculis, hendrerit quam eu, luctus mauris. Aenean faucibus justo nisi. Quisque eu lobortis ex. Donec pulvinar, est ac facilisis malesuada, velit diam lobortis dui,</p>
+
+        </div>
+     );
+}
+ 
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+// import Image from 'next/image'
+// import { Inter } from 'next/font/google'
+// import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-  <div>Hello world</div>
+  <div>
+    <h1>Hello world </h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel tellus vitae ex tempor dapibus quis feugiat dolor. Proin at pulvinar lorem. Nunc at sapien iaculis, hendrerit quam eu, luctus mauris. Aenean faucibus justo nisi. Quisque eu lobortis ex. Donec pulvinar, est ac facilisis malesuada, velit diam lobortis dui,</p>
+  </div>
   )
 }
