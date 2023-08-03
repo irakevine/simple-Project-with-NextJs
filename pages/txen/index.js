b57ee3003f6c8245b2adee45ce280d520a5cb1d6
irakevine <ikevine2016@gmail.com>
2023-08-04 01:42:41 +0200

    adding data

--- pages/txen/index.js
+++ pages/txen/index.js
@@ -1,8 +1,14 @@
 const Txen = () => {
     return ( 
+        <>
+        <Head>
+        <title>Txen List | Home</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
         <div>
             <h1>All Txen</h1>
         </div>
+        </>
      );
 }
  
