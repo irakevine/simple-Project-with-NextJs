a1753531bdd16e3fa9606f0cb74181c2fcff0357
irakevine <ikevine2016@gmail.com>
2023-08-04 09:35:20 +0200

    adding txen style

--- pages/txen/index.js
+++ pages/txen/index.js
@@ -1,4 +1,4 @@
-
+import styles from '../../styles/txen.module.css'
 export const getStaticProps = async() => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users')
     const data = await res.json();
@@ -16,8 +16,9 @@ const Txens = ({ txens }) => {
             {txens.map(txen =>(
                 <div key={txen.id}>
 
+            <a className={styles.single}>
          <h2>{txen.name}</h2>
-          
+             </a>
                 </div>
 
             ))}
