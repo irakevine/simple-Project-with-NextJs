13d5667daf84c7008eb26ae4ad82db07d84cedd3
irakevine <ikevine2016@gmail.com>
2023-08-04 11:28:23 +0200

    fetching each user details data

--- pages/txen/[id].js
+++ pages/txen/[id].js
@@ -1,24 +1,39 @@
-
-export const getStaticProps = async () =>{
-const res = await fetch('https://jsonplaceholder.typicode.com/users')
-const data = await res.json();
-
-const paths = data.map(txen =>{
-    return{
-        params: {id: txen.toString()}
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+  
+    // map data to an array of path objects with params (id)
+    const paths = data.map(txen => {
+      return {
+        params: { id: txen.id.toString() }
+      }
+    })
+  
+    return {
+      paths,
+      fallback: false
     }
-})
-return{
-    paths,
-    fallBack:false
-}
-}
-const Details = () => {
-    return ( 
-        <div>
-            <h1>Details page</h1>
-        </div>
-     );
-}
- 
-export default Details;
+  }
+  
+  export const getStaticProps = async (context) => {
+    const id = context.params.id;
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+    const data = await res.json();
+  
+    return {
+      props: { txen: data }
+    }
+  }
+  
+  const Details = ({ txen }) => {
+    return (
+      <div>
+        <h1>{ txen.name }</h1>
+        <p>{ txen.email }</p>
+        <p>{ txen.website }</p>
+        <p>{ txen.address.city }</p>
+      </div>
+    );
+  }
+  
+  export default Details;
